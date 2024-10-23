f6b7f2abdd0cc7c6cbe5877cad713ec84106453e
mpetillo <mpetillo@ku.edu>
2024-10-23 17:14:23 -0500

    adding server barebones

---
+++ backend/backend.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 5100;
+const localNetworkHost = '0.0.0.0';
+
+http.createServer(app);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request')
+})
+// Registers the app to use bodyParser to make our lives easier and avoid needing to decode json frequently.
+app.use(bodyParser.json());
+
+// Server definition
